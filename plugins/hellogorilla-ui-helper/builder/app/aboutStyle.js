5876b0bfad282ef829bdbe77fb974c9572def854
ruucm-working <ruucm.a@gmail.com>
2018-11-01 07:12:49 +0900

    pub: About
    third row, image, button

--- plugins/hellogorilla-ui-helper/builder/app/aboutStyle.js
+++ plugins/hellogorilla-ui-helper/builder/app/aboutStyle.js
@@ -54,29 +54,29 @@ injectGlobal`
 
 
 .hello-gorilla-first-row {
-  margin-top: ${wem2(94)};
+  margin-top: 94px
 }
 .hello-gorilla-left {
-    /* margin-top: ${wem2(94)}; */
+    /* margin-top: 94px */
 }
 .hello-gorilla-right {
-    /* margin-top: ${wem2(94)};  */
+    /* margin-top: 94px  */
     max-width: 528px;
 }
 .hello-gorilla-title {
   /* font-family: Lato; */
-  font-size: ${wem2(32)}
+  font-size: 32px;  
   font-weight: 300;
   line-height: 1.5;
   color: #231f20;
   }
 .hello-gorilla-sub-title {
-  font-size: ${wem2(20)};
+  font-size: 20px;
   color: #533c97;
   margin: unset;
 }
 .hello-gorilla-text {
-  font-size: ${wem2(16)};
+  font-size: 16px;
   line-height: 1.75;
   color: #231f20;
   margin: unset;
@@ -86,7 +86,30 @@ injectGlobal`
 }
 
 .hello-gorilla-second-row {
-  margin-top: ${wem2(192)};
+  margin-top: 192px;
 }
-  
+
+.hello-gorilla-third-row {
+  margin-top: 144px;
+}
+
+.hello-gorilla-button {
+  width: 144px;
+  height: 48px;
+  border-radius: 2px;
+  background-color: #805de9;
+  left: 50%;
+  position: relative;
+  transform: translateX(-50%);
+  color: white;
+  margin-top: 37px;
+  cursor:pointer;
+}
+.hello-gorilla-button-text {
+  position:absolute;
+  left: 50%;
+  top:50%;
+  transform:translate(-50%,-50%);
+  font-size:16px
+}
 `;
