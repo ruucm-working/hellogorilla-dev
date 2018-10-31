3b88056a41724d284de80913581b5557db4f0b9d
ruucm-working <ruucm.a@gmail.com>
2018-10-31 21:37:44 +0900

    pub: Product
    overlay

--- plugins/hellogorilla-ui-helper/builder/app/modules/Shop/components/ShopGrid/Product/index.js
+++ plugins/hellogorilla-ui-helper/builder/app/modules/Shop/components/ShopGrid/Product/index.js
@@ -33,6 +33,101 @@ const ImageWrapper = styled.div`
   height: ${wem2(360)};
 `;
 
+const Overlay = styled.div`
+  width: ${wem2(288)};
+  height: ${wem2(360)};
+  opacity: 0;
+  position: absolute;
+  top: 0;
+  cursor: pointer;
+  z-index: 10;
+  :hover {
+    opacity: 1;
+    background-color: rgba(128, 92, 232, 0.5);
+  }
+`;
+const SalePercentage = styled.div`
+  background: #533c97;
+  display: none;
+  position: absolute;
+  top: 0;
+  /* margin-top: ${wem2(96)}; */
+  /* margin-left: ${wem2(16)}; */
+  width: ${wem2(72)};
+  height: ${wem2(72)};
+  border-bottom-right-radius: 2px;
+  ${props =>
+    props.show &&
+    css`
+      display: block;
+    `};
+`;
+const SaleText = styled.div`
+  font-size: ${wem2(24)};
+  text-align: center;
+  color: #ffffff;
+  margin-top: ${wem2(12)};
+  ${props =>
+    props.small &&
+    css`
+      font-size: ${wem2(16)};
+      margin-top: ${wem2(8)};
+    `};
+`;
+const NewLabel = styled.div`
+  display: none;
+  background: #533c97;
+  position: absolute;
+  width: ${wem2(96)};
+  height: ${wem2(36)};
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  border-top-left-radius: 2px;
+  border-top-right-radius: 2px;
+  ${props =>
+    props.show &&
+    css`
+      display: block;
+    `};
+`;
+const NewLabelText = styled.div`
+  font-size: ${wem2(14)};
+  color: white;
+  text-transform: uppercase;
+  top: 50%;
+  position: absolute;
+  transform: translate(-50%, -50%);
+  left: 50%;
+`;
+
+const SoldOut = styled.div`
+  display: none;
+  position: absolute;
+  width: ${wem2(240)};
+  height: ${wem2(240)};
+  background-color: rgba(83, 60, 151, 0.8);
+  border-radius: 50%;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  ${props =>
+    props.show &&
+    css`
+      display: block;
+    `};
+`;
+const SoldOutLabel = styled.div`
+  line-height: 1.14;
+  font-size: 28px;
+  color: #ffffff;
+  text-align: center;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
+
 const TextWrapper = styled.div`
   text-align: center;
   margin-top: ${wem2(24)};
@@ -54,35 +149,6 @@ const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
-`;
-
-const SalePercentage = styled.div`
-  background: #533c97;
-  display: none;
-  position: absolute;
-  top: 0;
-  margin-top: ${wem2(96)};
-  margin-left: ${wem2(16)};
-  width: ${wem2(72)};
-  height: ${wem2(72)};
-  border-bottom-right-radius: 2px;
-  ${props =>
-    props.show &&
-    css`
-      display: block;
-    `};
-`;
-const SaleText = styled.div`
-  font-size: ${wem2(24)};
-  text-align: center;
-  color: #ffffff;
-  margin-top: ${wem2(12)};
-  ${props =>
-    props.small &&
-    css`
-      font-size: ${wem2(16)};
-      margin-top: ${wem2(8)};
-    `};
 `;
 
 const Price = styled.div`
@@ -107,69 +173,6 @@ const SalePrice = styled.div`
   color: #4a4a4a;
 `;
 
-const SoldOut = styled.div`
-  display: none;
-  position: absolute;
-  width: ${wem2(240)};
-  height: ${wem2(240)};
-  background-color: rgba(83, 60, 151, 0.8);
-  border-radius: 50%;
-  top: ${wem2(156)};
-  left: ${wem2(40)};
-  ${props =>
-    props.show &&
-    css`
-      display: block;
-    `};
-`;
-const SoldOutLabel = styled.div`
-  line-height: 1.14;
-  font-size: 28px;
-  color: #ffffff;
-  text-align: center;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-`;
-
-const NewLabel = styled.div`
-  display: none;
-  background: #533c97;
-  position: absolute;
-  width: ${wem2(96)};
-  height: ${wem2(36)};
-  top: ${wem2(420)};
-  left: ${wem2(112)};
-  border-top-left-radius: 2px;
-  border-top-right-radius: 2px;
-  ${props =>
-    props.show &&
-    css`
-      display: block;
-    `};
-`;
-const NewLabelText = styled.div`
-  font-size: ${wem2(14)};
-  color: white;
-  text-transform: uppercase;
-  top: 50%;
-  position: absolute;
-  transform: translate(-50%, -50%);
-  left: 50%;
-`;
-const Overlay = styled.div`
-  width: ${wem2(288)};
-  height: ${wem2(360)};
-  opacity: 0;
-  position: absolute;
-  top: 0;
-  :hover {
-    opacity: 1;
-    background-color: rgba(128, 92, 232, 0.5);
-  }
-`;
-
 const Product = props => {
   log("props", props);
   const { data, addToCart, index } = props;
@@ -182,7 +185,27 @@ const Product = props => {
               <ImageWrapper>
                 <Image src={data.images[0].src} />
               </ImageWrapper>
+
               <Overlay />
+              <SalePercentage show={data.sale_price}>
+                <SaleText>{(data.sale_price / data.price) * 100} %</SaleText>
+
+                <SaleText small>OFF</SaleText>
+              </SalePercentage>
+
+              <NewLabel show={data.tags.length}>
+                <NewLabelText>
+                  {data.tags[0] ? data.tags[0].name : ""}
+                </NewLabelText>
+              </NewLabel>
+
+              <SoldOut show={!data.in_stock}>
+                <SoldOutLabel>
+                  SOLD
+                  <br />
+                  OUT
+                </SoldOutLabel>
+              </SoldOut>
             </Center>
           </ProductWrapper>
         </Wrapper>
@@ -200,24 +223,6 @@ const Product = props => {
         </RegularPrice>
         <SalePrice>￦{commaPipe(data.price)}</SalePrice>
       </Price>
-
-      <SalePercentage show={data.sale_price}>
-        <SaleText>{(data.sale_price / data.price) * 100} %</SaleText>
-
-        <SaleText small>OFF</SaleText>
-      </SalePercentage>
-
-      <NewLabel show={data.tags.length}>
-        <NewLabelText>{data.tags[0] ? data.tags[0].name : ""}</NewLabelText>
-      </NewLabel>
-
-      <SoldOut show={!data.in_stock}>
-        <SoldOutLabel>
-          SOLD
-          <br />
-          OUT
-        </SoldOutLabel>
-      </SoldOut>
     </div>
   );
 };
