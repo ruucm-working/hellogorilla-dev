37cc9f00b577d81c4c8db2292ecf522b5c0fdd61
ruucm <ruucm.a@gmail.com>
2018-11-12 23:05:02 +0900

    db url

--- plugins/hellogorilla-ui-helper/builder/app/modules/DataContainer/consts.js
+++ plugins/hellogorilla-ui-helper/builder/app/modules/DataContainer/consts.js
@@ -1,5 +1,5 @@
-// const DATA_BASE_URL = 'https://hellogorilla.com/'
-const DATA_BASE_URL = 'http://hellogo.cafe24.com/'
+const DATA_BASE_URL = 'http://hellogo.co.kr'
+// const DATA_BASE_URL = 'http://hellogo.cafe24.com/'
 // const DATA_BASE_URL = 'http://d-hellogorilla.test/'
 
 const WP_ADMIN_ID = 'admin'
