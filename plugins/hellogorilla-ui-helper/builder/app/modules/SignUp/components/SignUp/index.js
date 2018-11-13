d47732a1eaeec2763a8dfc0ff1b246681e3a4746
ruucm <ruucm.a@gmail.com>
2018-11-13 09:07:22 +0900

    feat: translate
    SignUp

--- plugins/hellogorilla-ui-helper/builder/app/modules/SignUp/components/SignUp/index.js
+++ plugins/hellogorilla-ui-helper/builder/app/modules/SignUp/components/SignUp/index.js
@@ -1,19 +1,20 @@
-import React from "react";
+import React from 'react'
 
-import { compose, withState, lifecycle, withHandlers } from "recompose";
-import styled, { css } from "styled-components";
-import { wem, wem2 } from "ruucm-blocks/tools/mixins";
-import { Field, reduxForm } from "redux-form";
-import { log } from "ruucm-util";
-import validate from "./validate";
+import { compose, withState, lifecycle, withHandlers } from 'recompose'
+import styled, { css } from 'styled-components'
+import { wem, wem2 } from 'ruucm-blocks/tools/mixins'
+import { Field, reduxForm } from 'redux-form'
+import { log } from 'ruucm-util'
+import validate from './validate'
+import { _t } from '../../../shared/translate'
 
 const Wrap = styled.div`
   margin-top: ${wem2(96)};
   margin-left: ${wem2(480)};
   margin-right: ${wem2(480)};
-`;
+`
 
-const CloseBtn = styled.button``;
+const CloseBtn = styled.button``
 
 const Title = styled.h1`
   position: absolute;
@@ -21,21 +22,21 @@ const Title = styled.h1`
   transform: translateX(-50%);
   font-size: ${wem2(24)};
   color: #231f20;
-`;
+`
 
 const Form = styled.form`
   padding-top: ${wem2(48)};
-`;
+`
 
 const RenderFieldLabel = styled.div`
   display: block;
   font-size: ${wem2(11)};
   color: #231f20;
   margin-top: ${wem2(24)};
-`;
+`
 const PasswordmatchField = styled.div`
   display: none;
-`;
+`
 
 const WriteField = styled.input`
   margin-top: ${wem2(10)};
@@ -46,7 +47,7 @@ const WriteField = styled.input`
   font-size: ${wem2(14)};
   padding-left: ${wem2(20)};
   color: #b7b5b6;
-`;
+`
 const PhoneWriteField = styled.input`
   margin-top: ${wem2(10)};
   width: ${wem2(104)};
@@ -58,7 +59,7 @@ const PhoneWriteField = styled.input`
   color: #b7b5b6;
   display: inline-block;
   margin-right: ${wem2(10)};
-`;
+`
 const PhoneMatchField = styled.input`
   margin-top: ${wem2(12)};
   width: ${wem2(332)};
@@ -70,7 +71,7 @@ const PhoneMatchField = styled.input`
   color: #b7b5b6;
   display: inline-block;
   margin-right: ${wem2(10)};
-`;
+`
 
 const PassButton = styled.div`
   display: inline-block;
@@ -81,7 +82,7 @@ const PassButton = styled.div`
   cursor: pointer;
   position: relative;
   vertical-align: middle;
-`;
+`
 const PassText = styled.div`
   white-space: nowrap;
   font-size: ${wem2(14)};
@@ -90,33 +91,36 @@ const PassText = styled.div`
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
-`;
+`
 
-const RoleFieldLabel = styled.span``;
-const RadioFieldLabel = styled.span``;
-const CheckFieldLabel = styled.span``;
-const CheckFields = styled.div``;
-const HR = styled.hr``;
-const SelectAll = styled.input``;
-const SelectAllLabel = styled.span``;
+const RoleFieldLabel = styled.span``
+const RadioFieldLabel = styled.span``
+const CheckFieldLabel = styled.span``
+const CheckFields = styled.div``
+const HR = styled.hr``
+const SelectAll = styled.input``
+const SelectAllLabel = styled.span``
 
 const TimeRemain = styled.div`
   font-size: ${wem2(11)};
   margin-top: ${wem2(12)};
   margin-left: ${wem2(20)};
   color: #0eb780;
-`;
+`
 
 const Agree = styled.div`
   text-align: center;
   font-size: 13px;
   color: #231f20;
   margin-top: 48px;
-`;
+`
 
 const ErrorWrapper = styled.div`
-  color: #ee4230;
-`;
+  font-size: ${wem2(11)};
+  margin-top: ${wem2(12)};
+  margin-left: ${wem2(20)};
+  color: #0eb780;
+`
 
 const Button = styled.button`
   width: ${wem2(480)};
@@ -126,7 +130,7 @@ const Button = styled.button`
   position: relative;
   margin-top: ${wem2(20)};
   cursor: pointer;
-`;
+`
 const Next = styled.div`
   position: absolute;
   top: 50%;
@@ -134,7 +138,7 @@ const Next = styled.div`
   transform: translate(-50%, -50%);
   color: #ffffff;
   font-size: ${wem2(14)};
-`;
+`
 
 const Already = styled.div`
   font-size: ${wem2(13)};
@@ -144,7 +148,7 @@ const Already = styled.div`
   position: absolute;
   left: 50%;
   transform: translateX(-50%);
-`;
+`
 
 // redux-form
 const renderField = ({
@@ -152,25 +156,25 @@ const renderField = ({
   label,
   placeholder,
   type,
-  meta: { touched, error }
+  meta: { touched, error },
 }) => (
   <div>
     <RenderFieldLabel>{label}</RenderFieldLabel>
     <WriteField input {...input} placeholder={placeholder} type={type} />
   </div>
-);
+)
 const passwordmatchField = ({
   input,
   label,
   placeholder,
   type,
-  meta: { touched, error }
+  meta: { touched, error },
 }) => (
   <div>
     <PasswordmatchField>{label}</PasswordmatchField>
     <WriteField input {...input} placeholder={placeholder} type={type} />
   </div>
-);
+)
 const phoneField = ({
   input,
   input02,
@@ -178,7 +182,8 @@ const phoneField = ({
   label,
   placeholder,
   type,
-  meta: { touched, error }
+  current_lang,
+  meta: { touched, error },
 }) => (
   <div>
     <RenderFieldLabel>{label}</RenderFieldLabel>
@@ -189,17 +194,17 @@ const phoneField = ({
       <PassText
         onClick={() => {
           // alert('hey')
-          log("IMP", IMP);
+          log('IMP', IMP)
 
           // IMP.certification(param, callback) 호출
           IMP.certification(
             {
               // param
-              merchant_uid: "merchant_" + new Date().getTime() // 옵션 값
+              merchant_uid: 'merchant_' + new Date().getTime(), // 옵션 값
             },
             rsp => {
               // callback
-              log("rsp", rsp);
+              log('rsp', rsp)
               if (rsp.success) {
                 // ...,
                 // 인증 성공 시 로직,
@@ -210,62 +215,63 @@ const phoneField = ({
                 // ...
               }
             }
-          );
+          )
         }}
       >
-        인증번호 전송
+        {_t(current_lang, '인증번호 전송')}
       </PassText>
     </PassButton>
   </div>
-);
+)
 
 const phoneMatchField = ({
   input,
   label,
   placeholder,
   type,
-  meta: { touched, error }
+  current_lang,
+  meta: { touched, error },
 }) => (
   <div>
     {/* <RenderFieldLabel /> */}
     <PhoneMatchField input {...input} placeholder={placeholder} type={type} />
     <PassButton>
-      <PassText>인증번호 확인</PassText>
+      <PassText>{_t(current_lang, '인증번호 확인')}</PassText>
     </PassButton>
   </div>
-);
+)
 
 const loginField = ({
   input,
   label,
   placeholder,
   type,
-  meta: { touched, error }
+  meta: { touched, error },
 }) => (
   <div>
     <LoginField {...input} placeholder={placeholder} type={type} />
   </div>
-);
+)
 
 const roleField = ({ input, label, type, meta: { touched, error } }) => (
   <div>
     <RoleFieldLabel>{label}</RoleFieldLabel>
     <input {...input} type={type} />
   </div>
-);
+)
 const radioField = ({ input, label, type, meta: { touched, error } }) => (
   <div>
     <RadioFieldLabel>{label}</RadioFieldLabel>
     <input {...input} type={type} />
   </div>
-);
+)
 
 const checkField = ({ input, label, type, meta: { touched, error } }) => (
   <div>
     <CheckFieldLabel>{label}</CheckFieldLabel>
     <input {...input} type={type} />
   </div>
-);
+)
 
 // react
 const SignUp = ({
@@ -289,6 +295,9 @@ const SignUp = ({
   reset,
   handleSubmit,
   submitting,
+
+  // wordpress
+  current_lang,
   wpSignUp,
 
   ...props
@@ -296,17 +305,22 @@ const SignUp = ({
   return (
     <div>
       <Wrap>
-        <Title>회원가입</Title>
+        <Title>{_t(current_lang, '회원가입')}</Title>
 
         <Form
           onSubmit={handleSubmit(values => {
-            log("values", values);
-            if (validate(values)) {
-              wpSignUp(values, "customer", res => {
-                log("res", res);
-                alert("회원가입을 축하 합니다.\n홈으로 이동합니다. ");
-                window.location = "/";
-              });
+            log('values', values)
+            if (validate(values, current_lang)) {
+              wpSignUp(values, 'customer', res => {
+                log('res', res)
+                alert(
+                  _t(
+                    current_lang,
+                    '회원가입을 축하 합니다.\n홈으로 이동합니다.'
+                  )
+                )
+                window.location = '/'
+              })
             }
           })}
         >
@@ -314,14 +328,14 @@ const SignUp = ({
             <Field
               name="email"
               type="email"
-              label="이메일"
-              placeholder="이메일 (email@gmail.com) "
+              label={_t(current_lang, '이메일')}
+              placeholder="email@gmail.com"
               component={renderField}
             />
             <Field
               name="username"
               type="text"
-              label="이름"
+              label={_t(current_lang, '이름')}
               component={renderField}
             />
 
@@ -329,64 +343,71 @@ const SignUp = ({
               name="password"
               type="password"
               label="비밀번호"
-              placeholder="8-20자 이내로 입력해주세요."
+              label={_t(current_lang, '비밀번호')}
+              placeholder={_t(current_lang, '8-20자 이내로 입력해주세요.')}
               component={renderField}
             />
             <Field
               name="password_match"
               type="password"
-              // label="비밀번호(확인)"
-              placeholder="비밀번호 확인을 위해 재입력해주세요."
+              placeholder={_t(
+                current_lang,
+                '비밀번호 확인을 위해 재입력해주세요.'
+              )}
               component={passwordmatchField}
             />
 
             <Field
               name="phone"
               type="tel"
-              label="휴대폰"
+              label={_t(current_lang, '휴대폰')}
+              current_lang={current_lang}
               component={phoneField}
             />
 
             <Field
               name="phone_match"
               type="tel"
-              // label="인증번호를 입력해주세요."
-              placeholder="인증번호를 입력해주세요."
+              placeholder={_t(current_lang, '인증번호를 입력해주세요.')}
+              current_lang={current_lang}
               component={phoneMatchField}
             />
           </div>
           <TimeRemain>남은 시간 02:33</TimeRemain>
 
           <Agree>
-            가입 버튼을 누름과 동시에 이용약관에 동의한 것으로 간주합니다.
+            {_t(
+              current_lang,
+              '가입 버튼을 누름과 동시에 이용약관에 동의한 것으로 간주합니다.'
+            )}
           </Agree>
           <div>
             {error && <ErrorWrapper>{error}</ErrorWrapper>}
 
             <Button type="submit" disabled={submitting}>
-              <Next>가입하기</Next>
+              <Next>{_t(current_lang, '가입하기')}</Next>
             </Button>
           </div>
 
-          <Already>이미 계정이 있으신가요?</Already>
+          <Already>{_t(current_lang, '이미 계정이 있으신가요?')}</Already>
         </Form>
       </Wrap>
     </div>
-  );
-};
+  )
+}
 
 // Component enhancer
 const enhance = compose(
-  withState("modalOpen", "setModalOpen", false),
+  withState('modalOpen', 'setModalOpen', false),
 
   lifecycle({
     componentDidMount() {
-      IMP.init("imp72953913");
-    }
+      IMP.init('imp72953913')
+    },
   })
-);
+)
 export default enhance(
   reduxForm({
-    form: "HG_SIGNUP_FORM"
+    form: 'HG_SIGNUP_FORM',
   })(SignUp)
-);
+)
