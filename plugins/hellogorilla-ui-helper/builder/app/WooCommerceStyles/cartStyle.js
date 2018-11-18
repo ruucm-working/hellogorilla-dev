5a514e2a12b90c16fd8856335990c12c62563500
ruucm-working <ruucm.a@gmail.com>
2018-11-18 19:05:59 +0900

    pub: Cart
    item css

--- plugins/hellogorilla-ui-helper/builder/app/WooCommerceStyles/cartStyle.js
+++ plugins/hellogorilla-ui-helper/builder/app/WooCommerceStyles/cartStyle.js
@@ -5,14 +5,50 @@ import media from 'ruucm-blocks/tools/media'
 
 const style = css`
   .page-id-7 {
-    form {
-      margin-left: ${wem2(150)} !important;
-      width: ${wem2(564)} !important;
+    .woocommerce-cart-form {
+      /* margin-left: ${wem2(150)} !important;
+      width: ${wem2(564)} !important; */
+      min-width: 564px !important;
+      /* 상품정보 */
+      .product-remove {
+        vertical-align: middle;
+      }
+      .product-name {
+        display: inline-block;
+        padding: unset !important;
+        position:relative ;
+        height: 62px !important;
+        a {
+        position: absolute;
+        font-family: 'NanumSquareRoundWeb', sans-serif !important;
+        font-size: ${wem2(14)} !important;
+        color: #231f20 !important;
+        font-weight: 300;
+        }
+      }
+      .product-price {
+        display: inline-block !important;
+        padding: unset !important;
+        position:relative ;
+        height: 62px !important;
+        width: 100% !important;
+        span {
+          position: absolute;
+        }
+      }
+      /* 수량 */
+      .quantity {
+        a {
+          display: inline-block;
+        }
+      }
     }
     h2 {
       display: none;
     }
+    
 
+    /* 카트 계산서 */
     .cart-collaterals {
       margin-right: ${wem2(150)};
       width: ${wem2(324)} !important;
