f8c1c1b8a8e1d16ece13e168be42794db7620141
ruucm-working <ruucm.a@gmail.com>
2018-11-21 21:52:33 +0900

    pub: Address
    input box

--- plugins/hellogorilla-ui-helper/builder/app/WooCommerceStyles/addressStyle.js
+++ plugins/hellogorilla-ui-helper/builder/app/WooCommerceStyles/addressStyle.js
@@ -7,19 +7,25 @@ const style = css`
   .page-id-9 {
     /* address page css */
     .woocommerce-MyAccount-content {
-      p {
-        display: none;
-      }
       .col2-set {
         margin-top: unset;
-        .woocommerce-Address-title title {
-          font-size: ${wem2(24)};
-          font-weight: normal;
-          font-style: normal;
-          font-stretch: normal;
-          line-height: 1;
-          letter-spacing: normal;
-          color: #231f20;
+      }
+      .woocommerce-Address-title title h3 {
+        font-size: ${wem2(24)};
+        font-weight: normal;
+        font-style: normal;
+        font-stretch: normal;
+        line-height: 1;
+        letter-spacing: normal;
+        color: #231f20;
+      }
+      .woocommerce-address-fields {
+        margin-top: 40px;
+        .input-text {
+          width: 708px;
+          height: 52px;
+          border-radius: 2px;
+          background-color: rgba(244, 243, 243, 0.5);
         }
       }
     }
