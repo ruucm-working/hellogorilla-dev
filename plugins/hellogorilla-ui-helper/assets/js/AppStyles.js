4665ed096dbb1c8ec8b7aef3b835b809942ba2e4
ruucm-working <ruucm.a@gmail.com>
2018-11-23 21:18:13 +0900

    pub: Oderreceived
    font family

--- plugins/hellogorilla-ui-helper/assets/js/AppStyles.js
+++ plugins/hellogorilla-ui-helper/assets/js/AppStyles.js
@@ -379,7 +379,7 @@ var _media2 = _interopRequireDefault(_media);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var style = (0, _styledComponents.css)(['.page-id-8{.post-8{.woocommerce-notice{background:#533c97;}}}']);
+var style = (0, _styledComponents.css)(['.page-id-8{.post-8{font-family:\'NanumSquareRoundWeb\',sans-serif;.woocommerce-notice{background:#533c97;}.woocommerce-order-details{.woocommerce-order-details__title{font-size:24px;font-weight:normal;font-style:normal;font-stretch:normal;line-height:1;letter-spacing:normal;color:#231f20;}.woocommerce-table--order-details{thead{th{font-family:\'NanumSquareRoundWeb\',sans-serif;font-size:14px;font-weight:normal;font-style:normal;font-stretch:normal;line-height:1;letter-spacing:normal;color:#231f20;}}}}}}']);
 
 exports.default = style;
 
--- plugins/hellogorilla-ui-helper/builder/app/WooCommerceStyles/orderreceivedStyle.js
+++ plugins/hellogorilla-ui-helper/builder/app/WooCommerceStyles/orderreceivedStyle.js
@@ -6,8 +6,35 @@ import media from 'ruucm-blocks/tools/media'
 const style = css`
   .page-id-8 {
     .post-8 {
+      font-family: 'NanumSquareRoundWeb', sans-serif;
       .woocommerce-notice {
         background: #533c97;
+      }
+      /* 주문 상세 */
+      .woocommerce-order-details {
+        .woocommerce-order-details__title {
+          font-size: 24px;
+          font-weight: normal;
+          font-style: normal;
+          font-stretch: normal;
+          line-height: 1;
+          letter-spacing: normal;
+          color: #231f20;
+        }
+        .woocommerce-table--order-details {
+          thead {
+            th {
+              font-family: 'NanumSquareRoundWeb', sans-serif;
+              font-size: 14px;
+              font-weight: normal;
+              font-style: normal;
+              font-stretch: normal;
+              line-height: 1;
+              letter-spacing: normal;
+              color: #231f20;
+            }
+          }
+        }
       }
     }
   }
