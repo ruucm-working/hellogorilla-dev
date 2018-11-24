f6d8ded71a46bfbe35d7b29b370bf4bfa36b40d4
ruucm-working <ruucm.a@gmail.com>
2018-11-24 09:13:08 +0900

    pub: NewArtist
    border color

--- plugins/hellogorilla-ui-helper/builder/app/VCStyles/newartistStyle.js
+++ plugins/hellogorilla-ui-helper/builder/app/VCStyles/newartistStyle.js
@@ -9,6 +9,35 @@ const style = css`
     .site-content {
       max-width: 960px;
       margin: 0 auto;
+      margin-top: ${wem2(128)};
+      font-family: 'NanumSquareRoundWeb', sans-serif;
+
+      .NewArtist__Title-sc-1mlgdnv-3 {
+        font-family: 'NanumSquareRoundWeb', sans-serif;
+        font-size: 24px;
+        font-weight: normal;
+        font-style: normal;
+        font-stretch: normal;
+        line-height: 1;
+        letter-spacing: normal;
+        color: #231f20;
+      }
+      .NewArtist__Form-sc-1mlgdnv-5 {
+        input {
+          border: 1px solid #231f20;
+          border-radius: 2px;
+        }
+        .NewArtist__RederFieldLabel-sc-1mlgdnv-8 {
+          font-family: 'NanumSquareRoundWeb', sans-serif;
+          font-size: 11px;
+          font-weight: normal;
+          font-style: normal;
+          font-stretch: normal;
+          line-height: 1;
+          letter-spacing: normal;
+          color: #231f20;
+        }
+      }
     }
   }
 `
